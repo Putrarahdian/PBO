73fc36ff8808033c993868967e114f1d1fa12249
rahdian <152253719+Putrarahdian@users.noreply.github.com>
2024-07-14 20:18:25 +0800

    up

--- Komik.java
+++
@@ -1,7 +0,0 @@
-public class Komik {
-    private int volume;
-
-    public int getvolume(){
-        return this.volume;
-    }
-}
--- Tamu.java
+++
@@ -1,7 +0,0 @@
-public class Tamu {
-    private String sisaMasaAktif;
-
-    public String getAktif(){
-        return this.sisaMasaAktif;
-    }
-}
